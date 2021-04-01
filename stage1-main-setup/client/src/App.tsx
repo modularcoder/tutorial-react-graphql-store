22d71f8e23ac63258859ef39d8e5f45959269065
Gevorg Harutyunyan <gevharut@gmail.com>
2021-04-02 01:23:59 +0400

    Minor updates

--- stage1-main-setup/client/src/App.tsx
+++ stage1-main-setup/client/src/App.tsx
@@ -9,12 +9,9 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Bookstore tutorial with ReactJS, GraphQL and TypeScript</p>
         <p>
           <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
