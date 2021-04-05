994a6bfa43ffaa93d3e474c240237d4ac321b610
Gevorg Harutyunyan <gevharut@gmail.com>
2021-04-06 03:44:13 +0400

    Configs

--- stage1-main-setup/stage1-client/src/App.tsx
+++ stage1-main-setup/stage1-client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-white object-center bg-indigo-400">
       <h1>Bookstore tutorial with ReactJS, TailwindCSS, GraphQL and TypeScript</h1>
     </div>
   )
