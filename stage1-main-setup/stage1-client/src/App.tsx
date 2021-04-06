55421d7c9cece3e1a450fd980103cf4a7da08c53
Gevorg Harutyunyan <gevharut@gmail.com>
2021-04-06 04:41:42 +0400

    Initial styles

--- stage1-main-setup/stage1-client/src/App.tsx
+++ stage1-main-setup/stage1-client/src/App.tsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="App text-white object-center bg-indigo-400">
-      <h1>Bookstore tutorial with ReactJS, TailwindCSS, GraphQL and TypeScript</h1>
+    <div
+      className="
+        App
+        h-screen
+        w-full
+
+        flex
+        justify-center
+        items-center
+        text-white
+
+        bg-gradient-to-br
+        from-purple-500
+        to-indigo-500
+      "
+    >
+      <div className="container text-center">
+        <h1 className="text-3xl font-semibold mb-2 text-shadow">
+          Bookstore tutorial with ReactJS, TailwindCSS, GraphQL and TypeScript
+        </h1>
+      </div>
     </div>
   )
 }
