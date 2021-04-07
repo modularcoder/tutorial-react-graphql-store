d5ea2d57032215636629f8083c4ce50c6d2f9d78
Gevorg Harutyunyan <gevharut@gmail.com>
2021-04-07 06:30:27 +0400

    Update proxy port

--- stage0-draft/proxy.js
+++ stage0-draft/proxy.js
@@ -9,4 +9,4 @@ app.use('/api', proxy({ target: 'http://localhost:4000', changeOrigin: true, ws:
 
 app.use('/app', proxy({ target: 'http://localhost:3000', changeOrigin: true, ws: true }))
 
-app.listen(80)
+app.listen(8080)
